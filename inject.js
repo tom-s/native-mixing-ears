0201c9c8f554628af885f6d77317b789d0c3c1c8
Thomas Schell <thom.schell@gmail.com>
2016-06-07 01:25:38 +0200

    FINALLY FUCKING WORKING

--- inject.js
+++ inject.js
@@ -1,13 +1,13 @@
+import drumsSound from './assets/drums'
+import decodeArrayBuffer from './utils/base64Binary'
 
-const inject = (window) => {
-
+const inject = (window, sounds, decodeArrayBuffer) => {
   // create web audio api context
   let audioCtx = new (window.AudioContext || window.webkitAudioContext)()
 
 
   const play = (payload) => {
-    const { sound } = payload
-    alert("play" + sound)
+    const { soundId } = payload
 
     const playSound = (buffer) => {
       const mySource = audioCtx.createBufferSource()
@@ -16,31 +16,17 @@ const inject = (window) => {
       mySource.start(0)
     }
 
-    const loadSound = (url) => {
-      const request = new XMLHttpRequest()
-      request.open('GET', url, true)
-      request.responseType = 'arraybuffer'
-
-      // Decode asynchronously
-      request.onload = function() {
-        alert('response status' + request.status)
-        audioCtx.decodeAudioData(request.response, (buffer) => {
-          alert('buffer' + buffer)
+    const loadSound = (soundId) => {
+      const buff = decodeArrayBuffer(sounds[soundId])
+      audioCtx.decodeAudioData(buff, (buffer) => {
           playSound(buffer)
         }, (e) => {
           alert('error')
           alert('error decoding' + e)
         })
-      }
-      request.onerror = () => {
-        alert("error loading file")
-      }
-
-      request.send();
     }
 
-    loadSound('/android_asset/www/drums.wav')
-    //loadSound('http://freewavesamples.com/files/Korg-DW-8000-Noise-Snare.wav')
+    loadSound('drums')
   }
 
   const pause = (payload) => {
@@ -66,7 +52,11 @@ const inject = (window) => {
  // add dependancies
 const wrappedInject = `
     var test = ${inject}
-    test(window, null)
+    var sounds = {
+     "drums": "${drumsSound}"
+    };
+    var decodeArrayBuffer = ${decodeArrayBuffer}
+    test(window, sounds, decodeArrayBuffer)
 `
 
 export default wrappedInject
