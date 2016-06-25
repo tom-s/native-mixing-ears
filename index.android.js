41ddf3364b4c49cf9b023e5b67b5343d5bf1a32a
Thomas Schell <thom.schell@gmail.com>
2016-06-25 19:53:31 +0200

    trying to make it work

--- index.android.js
+++ index.android.js
@@ -1,9 +1,9 @@
-import Reactotron from 'reactotron'
-import Root from '3dears/src/containers/Root'
+//import Reactotron from 'reactotron'
+import Root from './src/containers/Root'
 import { AppRegistry } from 'react-native'
 
 // Reactotron
-Reactotron.connect({enabled: __DEV__})
+//Reactotron.connect({enabled: __DEV__})
 
 // Init app
 AppRegistry.registerComponent('3dears', () => Root)
--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,17 +1,21 @@
-import React from 'react-native'
-import { Provider } from 'react-redux'
-import configureStore from '3dears/src/store/configure-store'
-import App from '3dears/src/containers/App'
+import { Component } from 'react'
+//import { Provider } from 'react-redux'
+//import configureStore from '3dears/src/store/configure-store'
+//import App from '3dears/src/containers/App'
 
-const store = configureStore()
+//const store = configureStore()
 
-class Root extends React.Component {
+class Root extends Component {
   render() {
+    return (
+      <div>hourra</div>
+    )
+    /*
     return (
       <Provider store={store}>
         {() => <App />}
       </Provider>
-    )
+    )*/
   }
 }
 
