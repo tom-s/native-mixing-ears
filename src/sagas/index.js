127c47ea56596a0e18cd7dd2b20dfc631d09c593
tom-s <thom.schell@gmail.com>
2016-07-16 22:35:50 +0200

    added codepush saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,1 +1,10 @@
-export default []
+import { fork } from 'redux-saga/effects'
+import codePushSaga from "react-native-code-push-saga"
+
+function* rootSaga() {
+  yield [
+    fork(codePushSaga)
+  ]
+}
+
+export default rootSaga
