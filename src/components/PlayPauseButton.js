7b4524319e9242719e2cab7ab736448d93d4ff1c
Thomas Schell <thom.schell@gmail.com>
2016-07-21 17:17:02 +0200

    animate play pause button

--- src/components/PlayPauseButton.js
+++ src/components/PlayPauseButton.js
@@ -1,22 +1,65 @@
 import React, { Component } from 'react'
-import { Motion, StaggeredMotion, spring } from 'react-motion'
-import Svg,{
-    Path
-} from 'react-native-svg'
-const animationConfig = {stiffness: 170, damping: 9}
-const fadeConfig = {stiffness: 10, damping: 9}
+import {TouchableWithoutFeedback } from 'react-native'
+import { Motion, spring } from 'react-motion'
+import Svg,{ Path} from 'react-native-svg'
+const animationConfig = {stiffness: 70, damping: 9}
+//const fadeConfig = {stiffness: 10, damping: 9}
 
 class PlayPauseButton extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      paused: true,
+      pause: {
+        previousOpacity: 0,
+        opacity: 0
+      },
+      play: {
+        previousOpacity: 1,
+        opacity: 1
+      }
+    }
   }
+
   render() {
+    const { pause, play } = this.state
     return (
-      <Svg width="200" height="200" viewBox="0 0 36 36" >
-          <Path d="M11,10 L17,10 17,26 11,26 M20,10 L26,10 26,26 20,26" />
-          <Path d="M11,10 L18,13.74 18,22.28 11,26 M18,13.74 L26,18 26,18 18,22.28" />
-      </Svg>
+      <Motion defaultStyle={{playopacity: play.previousOpacity, pauseopacity: pause.previousOpacity }}
+          style={{playopacity: spring(play.opacity, animationConfig), pauseopacity: spring(pause.opacity, animationConfig)}}>
+          { newStyle =>   (
+            <TouchableWithoutFeedback onPress={this._togglePaused.bind(this)}>
+              <Svg width="200" height="200" viewBox="0 0 36 36">
+                <Path d="M11,10 L17,10 17,26 11,26 M20,10 L26,10 26,26 20,26" fillOpacity={newStyle.playopacity} />
+                <Path d="M11,10 L18,13.74 18,22.28 11,26 M18,13.74 L26,18 26,18 18,22.28" fillOpacity={newStyle.pauseopacity} />
+              </Svg>
+            </TouchableWithoutFeedback>
+          )}
+      </Motion>
     )
+  }
+
+  _togglePaused() {
+    console.log("set paused to ", !paused)
+
+    const { paused, pause, play } = this.state
+    if (paused) {
+      pause.previousOpacity = 0
+      pause.opacity = 1
+      play.previousOpacity = 1
+      play.opacity = 0
+    } else {
+      pause.previousOpacity = 1
+      pause.opacity = 0
+      play.previousOpacity = 0
+      play.opacity = 1
+    }
+
+
+    this.setState({
+      paused: !paused,
+      play,
+      pause
+    })
   }
 }
 
