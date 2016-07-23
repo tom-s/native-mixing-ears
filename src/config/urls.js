de1c96f0db320f506edb242cddaa01f14cf3b32a
Thomas Schell <thom.schell@gmail.com>
2016-07-23 14:14:06 +0200

    over with private database sync

---
+++ src/config/urls.js
@@ -0,0 +1,1 @@
+export const POUCH_SERVER_IP = '192.168.0.12'
---
+++ src/utils/pouchdb.js
@@ -0,0 +1,31 @@
+import PouchDB from 'pouchdb-react-native'
+import PouchAsync from 'pouchdb-adapter-asyncstorage'
+import { POUCH_SERVER_IP } from 'nativeMixing/src/config/urls'
+
+PouchDB.plugin(PouchAsync)
+
+export const createDbs = (uniqueId) => {
+  const localDB = new PouchDB(uniqueId, {adapter: 'asyncstorage'})
+  const remoteDB = new PouchDB(`http://${POUCH_SERVER_IP}:5984/${uniqueId}`)
+  return { localDB, remoteDB }
+}
+
+export const dbsSync = (localDB, remoteDB) => {
+  return localDB.sync(remoteDB, {
+    live: false,
+    retry: true
+  }).then(() => {
+    return true
+  }).catch(() => {
+    return false
+  })
+}
+
+export const dbGet = (db, prop) => {
+  return db.get(prop).then(res => res).catch(() => null)
+}
+
+export const dbPut = (db, doc) => {
+  return db.put(doc).then(() => doc).catch(() => null)
+}
+
